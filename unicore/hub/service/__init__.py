55dc67b3faff6b97da501d7bb5a4ae5dd0a5939e
Rizmari Versfeld <rizziepit@gmail.com>
2015-03-02 15:36:49 +0200

    get/post user api

--- unicore/hub/service/__init__.py
+++ unicore/hub/service/__init__.py
@@ -1,3 +1,5 @@
+from pyramid.authentication import BasicAuthAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 from sqlalchemy.orm import sessionmaker
@@ -30,9 +32,19 @@ def main(global_config, **settings):
 
     # sqlalchemy setup
     config.scan('%s.models' % __name__)
+    config.scan('%s.user_api' % __name__)
     engine = engine_from_config(settings)
     config.registry.dbmaker = sessionmaker(bind=engine)
     # NOTE: db session is tied to request lifespan
     config.add_request_method(db, reify=True)
+
+    # auth setup
+    from unicore.hub.service.models import App
+    authn_policy = BasicAuthAuthenticationPolicy(
+        check=App.authenticate,
+        realm='apps')
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
 
     return config.make_wsgi_app()
--- unicore/hub/service/models.py
+++ unicore/hub/service/models.py
@@ -11,8 +11,28 @@ class User(Base):
     password = Column(PasswordType(schemes=['pbkdf2_sha256']))
     app_data = Column(JSONType)
 
+    @classmethod
+    def authenticate(cls, username, password, request):
+        user = request.db.query(cls) \
+            .filter(cls.username == username) \
+            .first()
+
+        if user is not None and user.password == password:
+            return (user.id, )
+
+        return None
+
 
 class App(Base):
     __tablename__ = 'apps'
     id = Column(Integer, primary_key=True)
     password = Column(PasswordType(schemes=['pbkdf2_sha256']))
+
+    @classmethod
+    def authenticate(cls, app_id, password, request):
+        app = request.db.query(cls).get(app_id)
+
+        if app is not None and app.password == password:
+            return (app_id, )
+
+        return None
